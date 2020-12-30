3aaef9be18df4185daf4ff18b18bcff797b2e03d
jondkelley <jonathan.kelley@logdna.com>
2020-12-30 17:35:49 -0600

    Fix setup.py version

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
 from setuptools import setup, find_packages
 from sys import path
 
-from answerbook_webhook_test import __version__
-
 path.insert(0, '.')
 
 NAME = "logdna_line_export"
 
 if __name__ == "__main__":
-    with open('requirements.txt') as f:
-        requirements = f.read().splitlines()
-
     setup(
         name=NAME,
-        version=__version__,
+        version="0.0.2",
         author="Jonathan Kelley",
         author_email="jonathan.kelley@logdna.com",
         url="https://github.com/jondkelley/logdna_line_export_cli",
@@ -21,8 +16,8 @@ if __name__ == "__main__":
         packages=find_packages(),
         package_dir={NAME: NAME},
         description="logdna_line_export - Just a simple webhook test app",
-
-        install_requires=requirements,
+        include_package_data=True,
+        install_requires=['Click','requests','python-dateutil'],
 
         entry_points={
             'console_scripts': ['logdna_line_export = logdna_line_export.main:entry'],
